d06fef98f594c4f8cd22cc4d2d932cb1d935b212
Sylvain Corsini <sylvain.corsini@protonmail.com>
2021-11-25 20:49:05 +0100

    feat: add Footer block

---
+++ src/lib/blocks/FooterBlock.svelte
@@ -0,0 +1,37 @@
+<section class='max-w-6xl mx-auto pt-16 pb-4 bg-fpn-blue text-white'>
+	<div class='mx-auto max-w-4xl'>
+		<div class='grid grid-cols-2 gap-24'>
+			<div>
+				<p class='text-4xl text-white font-extrabold'>
+					Perce<br/>
+					Neige
+				</p>
+				<p class='mt-4 text-sm'>
+					Fondation reconnue d’utilité publique qui agit en faveur des enfants et adultes
+					touchés par une déficience mentale, un handicap physique ou psychique.<br/>
+					Créée en 1966 à l’initiative de l’acteur Lino Ventura.
+				</p>
+			</div>
+			<div class=''>
+				<div class='flex justify-between items-center'>
+					<p>Suivez-nous</p>
+					<ul>
+						<li class='inline-block'><img style='width: 40px;' src='/icon-facebook.svg' alt='icone facebook' /></li>
+						<li class='inline-block'><img style='width: 40px;' src='/icon-twitter.svg' alt='icone twitter' /></li>
+						<li class='inline-block'><img style='width: 40px;' src='/icon-youtube.svg' alt='icone youtube' /></li>
+						<li class='inline-block'><img style='width: 40px;' src='/icon-instagram.svg' alt='icone instagram' /></li>
+						<li class='inline-block'><img style='width: 40px;' src='/icon-linkedin.svg' alt='icone linkedin' /></li>
+					</ul>
+				</div>
+				<div class='mt-10'>
+					<img class='mx-auto' style='width: 90px;' src='icon-don_en_confiance.png' />
+				</div>
+			</div>
+		</div>
+		<ul class='text-xs text-center mt-12'>
+			<li class='inline-block'>Perce-Neige 2021</li>
+			<li class='inline-block'>Mentions légales</li>
+			<li class='inline-block'>Gestion des cookies</li>
+		</ul>
+	</div>
+</section>
