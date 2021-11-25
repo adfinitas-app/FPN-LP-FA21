0644e6b43d3bd4dda2c8776064763e980311e0d1
Sylvain Corsini <sylvain.corsini@protonmail.com>
2021-11-25 14:05:22 +0100

    feat: change text size

--- src/lib/blocks/HeaderBlock.svelte
+++ src/lib/blocks/HeaderBlock.svelte
@@ -6,12 +6,12 @@
 	<img src='/photo-header.png' alt='Image' />
 	<div class='text-white text-center my-14'>
 		<PlaceholderWithCorner>
-			<h1 class='text-6xl font-extrabold'>
-				<span class='text-7xl'>30 jours</span> pour faire
+			<h1 class='text-2xl font-extrabold'>
+				<span class='text-4xl'>30 jours</span> pour faire
 				place aux personnes en situation de handicap
 			</h1>
 		</PlaceholderWithCorner>
-		<p class='text-2xl italic mt-10 font-bold'>
+		<p class='text-lg italic mt-10 font-bold'>
 			Les préjugés sont les premiers obstacles qui empêchent les personnes
 			en situation de handicap mental d’être pleinement respectées,
 			reconnues et intégrées. Aujourd’hui pour les aider à vivre sereinement au sein de la société,
