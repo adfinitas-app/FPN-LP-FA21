972ca686dcfa36e7ded917686f15ad0c46f9a2fd
Sylvain Corsini <sylvain.corsini@protonmail.com>
2021-11-26 09:53:12 +0100

    feat: add favicon, title, metas

--- src/lib/block-prejuge-config.js
+++ src/lib/block-prejuge-config.js
@@ -20,7 +20,7 @@ export const grid_options = [
 	},
 	{
 		type: 'prejuge',
-		title: 'Le handicap mental, c’est forcément de naissance.',
+		title: '<span class="relative">L</span>e handicap mental, c’est forcément de naissance.',
 		subtitle: 'Plus d’1 personne sur 10 accompagnée par Perce-Neige est handicapée suite à un accident, un traumatisme ou une maladie rare.',
 		testimonial: 'La vie de Pauline a basculé à l’âge de 6 ans, quand elle a été renversée par une moto sur la voie publique. Après plusieurs mois de coma, la petite fille se réveille enfin mais ses capacités mentales et motrices sont gravement altérées. Sa vie dite « ordinaire » s’est arrêtée, en décembre 1999.<br/>« Il est important de dire que ce qui s’est produit pour notre famille il y a plus de 20 ans peut hélas arriver à n’importe qui, n’importe quand » Isabelle, mère de Pauline.',
 		solution_title: 'Perce Neige apporte des solutions adaptées',
@@ -29,7 +29,7 @@ export const grid_options = [
 	},
 	{
 		type: 'prejuge',
-		title: 'Les personnes handicapées mentales n’ont pas conscience de leur différence.',
+		title: '<span class="relative">L</span>es personnes handicapées mentales n’ont pas conscience de leur différence.',
 		subtitle: 'Au contraire, non seulement elles ont conscience de leur différence mais en souffrent énormément.',
 		solution_title: 'Perce-Neige favorise l’épanouissement personnel',
 		solution_text: 'Parce que nous savons que les personnes en situation de handicap souffrent du regard et des préjugés portés sur elles, nous les entourons de toute notre bienveillance et favorisons leur épanouissement grâce à leur participation à des activités culturelles, sportives, manuelles. L’intégration des Maisons Perce-Neige dans la vie locale encourage l’inclusion des personnes handicapées dans notre société.',
@@ -37,7 +37,7 @@ export const grid_options = [
 	},
 	{
 		type: 'prejuge',
-		title: 'Les personnes handicapées mentales ne vivent pas très longtemps.',
+		title: '<span class="relative">L</span>es personnes handicapées mentales ne vivent pas très longtemps.',
 		subtitle: 'Depuis le début du XXe siècle, les personnes handicapées mentales ont gagné plus de trente années d’espérance de vie.',
 		solution_title: 'Perce-Neige accompagne le vieillissement de ses résidents',
 		solution_text: 'Pour accompagner le vieillissement des personnes handicapées mentales, Perce-Neige forme son personnel à détecter des signes avant-coureurs de certaines pathologies et à maintenir les capacités motrices et cognitives des résidents. Notre Fondation a créé plusieurs Maisons des aînés pour les accueillir et leur assurer un accompagnement adapté.',
@@ -45,7 +45,7 @@ export const grid_options = [
 	},
 	{
 		type: 'prejuge',
-		title: 'En France, on en fait déjà assez pour les personnes handicapées !',
+		title: '<span class="relative">E</span>n France, on en fait déjà assez pour les personnes handicapées !',
 		subtitle: 'Plus de 40 000 personnes handicapées sont toujours sans solution d’accueil en France.',
 		solution_title: 'Les actions de Perce-Neige pour aider les familles en souffrance',
 		solution_text: 'Perce-Neige accompagne au quotidien près de 1200 résidents et gère autant de demandes en liste d’attente !<br/><br/>Au sein de Perce-Neige, une cellule d’aide aux familles en difficulté assure une permanence téléphonique pour les conseiller et leur apporter notre expertise. Les familles peuvent également avoir recours en Ile-de-France et dans le Vaucluse aux services d’équipes mobiles qui interviennent en support à leur domicile et bénéficier, le cas échéant, d’accueil en demi-journées dans les Maisons Perce-Neige de proximité. Enfin, le programme « Classes Soleil » en maternelle, soutenu par Perce-Neige, offre aux enfants autistes « non-verbaux » la possibilité de suivre un enseignement adapté à leur handicap et de rejoindre par la suite une classe élémentaire ordinaire.',
