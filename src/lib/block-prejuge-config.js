2ffba5524136c020f423818ee50fda4929a62782
Arnaud Masselin <web@adfinitas.fr>
2021-11-30 15:40:23 +0000

    Updated 'src/lib/block-prejuge-config.js' via CloudCannon

--- src/lib/block-prejuge-config.js
+++ src/lib/block-prejuge-config.js
@@ -1,15 +1,15 @@
 export const tab_options = [
 	{
 		percentage: 30,
-		text: '30% pensent qu’une personne handicapée mentale ne peut pas être autonome.'
+		text: 'pensent qu’une personne handicapée mentale ne peut pas être autonome.'
 	},
 	{
 		percentage: 23,
-		text: '23% assurent qu’un enfant handicapé mental ne peut pas être scolarisé avec les autres enfants.'
+		text: 'assurent qu’un enfant handicapé mental ne peut pas être scolarisé avec les autres enfants.'
 	},
 	{
 		percentage: 17,
-		text: '17% disent que les personnes handicapées ne peuvent pas exercer une activité professionnelle.'
+		text: 'disent que les personnes handicapées ne peuvent pas exercer une activité professionnelle.'
 	}
 ];
 
@@ -21,7 +21,7 @@ export const grid_options = [
 	{
 		type: 'prejuge',
 		title: '<span class="relative">L</span>e handicap mental, c’est forcément de naissance.',
-		subtitle: 'Plus d’1 personne sur 10 accompagnée par Perce-Neige est handicapée à la suite d’un accident, d’un traumatisme ou d’une maladie rare.',
+		subtitle: 'FAUX ! Plus d’1 personne sur 10 accompagnée par Perce-Neige est handicapée à la suite d’un accident, d’un traumatisme ou d’une maladie rare.',
 		testimonial: 'La vie de Pauline a basculé à l’âge de 6 ans, quand elle a été renversée par une moto sur la voie publique. Après plusieurs mois de coma, la petite fille se réveille enfin mais ses capacités mentales et motrices sont gravement altérées. Sa vie dite « ordinaire » s’est arrêtée, en décembre 1999.<br/>« Il est important de dire que ce qui s’est produit pour notre famille il y a plus de 20 ans peut hélas arriver à n’importe qui, n’importe quand... » Isabelle, mère de Pauline.',
 		solution_title: 'Perce-Neige apporte des solutions adaptées',
 		solution_text: 'Face au manque de structures, Perce-Neige créé différents types d’établissements médico-sociaux avec des accompagnements spécifiques, pour les personnes avec autisme, les personnes handicapées psychiques ou physiques.',
@@ -30,7 +30,7 @@ export const grid_options = [
 	{
 		type: 'prejuge',
 		title: '<span class="relative">L</span>es personnes handicapées mentales n’ont pas conscience de leur différence.',
-		subtitle: 'Au contraire, non seulement elles ont conscience de leur différence mais en souffrent énormément.',
+		subtitle: 'FAUX ! non seulement elles ont conscience de leur différence mais en souffrent énormément.',
 		solution_title: 'Perce-Neige favorise l’épanouissement personnel',
 		solution_text: 'Parce que nous savons que les personnes en situation de handicap souffrent du regard et des préjugés portés sur elles, nous les entourons de toute notre bienveillance et favorisons leur épanouissement grâce à leur participation à des activités culturelles, sportives, manuelles. L’intégration des Maisons Perce-Neige dans la vie locale encourage l’inclusion des personnes handicapées dans notre société.',
 		precta_text: 'Faites un don à Perce-Neige pour aider les personnes handicapées à vivre pleinement et sereinement !'
@@ -38,7 +38,7 @@ export const grid_options = [
 	{
 		type: 'prejuge',
 		title: '<span class="relative">L</span>es personnes handicapées mentales ne vivent pas très longtemps.',
-		subtitle: 'Depuis le début du XXe siècle, les personnes handicapées mentales ont gagné plus de trente années d’espérance de vie.',
+		subtitle: 'FAUX ! Depuis le début du XXe siècle, les personnes handicapées mentales ont gagné plus de trente années d’espérance de vie.',
 		solution_title: 'Perce-Neige accompagne le vieillissement de ses résidents',
 		solution_text: 'Pour accompagner le vieillissement des personnes handicapées mentales, Perce-Neige forme son personnel à détecter des signes avant-coureurs de certaines pathologies et à maintenir les capacités motrices et cognitives des résidents. Notre Fondation a créé plusieurs Maisons des aînés pour les accueillir et leur assurer un accompagnement adapté.',
 		precta_text: 'Grâce à votre soutien, Perce-Neige pourra ouvrir de nouvelles Maisons des aînés pour accompagner les personnes handicapées vieillissantes dans la dignité.'
@@ -46,7 +46,7 @@ export const grid_options = [
 	{
 		type: 'prejuge',
 		title: '<span class="relative">E</span>n France, on en fait déjà assez pour les personnes handicapées !',
-		subtitle: 'Plus de 40 000 personnes handicapées sont toujours sans solution d’accueil en France.',
+		subtitle: 'FAUX ! Plus de 40 000 personnes handicapées sont toujours sans solution d’accueil en France.',
 		solution_title: 'Les actions de Perce-Neige pour aider les familles en souffrance',
 		solution_text: 'Perce-Neige accompagne au quotidien près de 1200 résidents et gère autant de demandes en liste d’attente...<br/><br/>Au sein de Perce-Neige, une cellule d’aide aux familles en difficulté assure une permanence téléphonique pour les conseiller et leur apporter notre expertise. Les familles peuvent également avoir recours en Ile-de-France et dans le Vaucluse aux services d’équipes mobiles qui interviennent en support à leur domicile et bénéficier, le cas échéant, d’accueil en demi-journées dans les Maisons Perce-Neige de proximité. Enfin, le programme « Classes Soleil » en maternelle, soutenu par Perce-Neige, offre aux enfants autistes « non-verbaux » la possibilité de suivre un enseignement adapté à leur handicap et de rejoindre par la suite une classe élémentaire ordinaire.',
 		precta_text: 'Votre générosité est indispensable pour répondre aux besoins de milliers de personnes sans solution d’accueil adapté. Merci d’avance pour votre don !'
