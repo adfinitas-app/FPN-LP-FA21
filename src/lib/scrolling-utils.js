4658ab8df49c337e71dc7015f4df33bf4fa19e9a
Sylvain Corsini <sylvain.corsini@protonmail.com>
2021-11-30 12:31:14 +0100

    fix: scrolling

--- src/lib/scrolling-utils.js
+++ src/lib/scrolling-utils.js
@@ -1,25 +1,7 @@
-let supportsPassive = false;
-try {
-	window.addEventListener('test', null, Object.defineProperty({}, 'passive', {
-		get: function() {
-			supportsPassive = true;
-		}
-	}));
-} catch (e) {
-}
-
-let wheelOpt = supportsPassive ? { passive: false } : false;
-
-function preventDefault(e) {
-	e.preventDefault();
-}
-
 export function disableScroll() {
 	document.body.classList.add('overflow-hidden', 'h-full', 'md:overflow-auto', 'md:h-auto');
-	document.body.addEventListener('touchmove', preventDefault, wheelOpt);
 }
 
 export function enableScroll() {
 	document.body.classList.remove('overflow-hidden', 'h-full', 'md:overflow-auto', 'md:h-auto');
-	document.body.removeEventListener('touchmove', preventDefault, wheelOpt);
 }
